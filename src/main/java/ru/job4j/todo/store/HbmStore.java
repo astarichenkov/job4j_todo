e79cec130ea2da880b287528074689a701de2d95
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-12-17 00:48:09 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/store/HbmStore.java
+++ src/main/java/ru/job4j/todo/store/HbmStore.java
@@ -9,6 +9,7 @@ import ru.job4j.todo.model.Item;
 
 import javax.persistence.Query;
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmStore implements Store, AutoCloseable {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -29,47 +30,45 @@ public class HbmStore implements Store, AutoCloseable {
 
     @Override
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from ru.job4j.todo.model.Item ORDER BY id").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.todo.model.Item ORDER BY id").list()
+        );
     }
 
     @Override
     public Item add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return (Item) this.tx(
+                session -> session.save(item)
+        );
     }
 
     @Override
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.get(Item.class, id)
+        );
     }
 
     @Override
     public int setDone(int id) {
+        return this.tx(
+                session -> {
+                    return session.createQuery(
+                                    "update Item i set i.done = :newDone where i.id = :fId"
+                            ).setParameter("newDone", true)
+                            .setParameter("fId", id)
+                            .executeUpdate();
+
+                });
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
         Session session = sf.openSession();
         session.beginTransaction();
-        Query query = session.createQuery(
-                "update Item i set i.done = :newDone where i.id = :fId"
-        );
-        query.setParameter("newDone", true);
-        query.setParameter("fId", id);
-        int rsl = query.executeUpdate();
+        T result = command.apply(session);
         session.getTransaction().commit();
         session.close();
-        return rsl;
+        return result;
     }
 
     @Override
